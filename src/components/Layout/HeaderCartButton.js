a073b363735c0544de48d2edd0690333c19c67df
Vishesh Panchal <visheshpanchal145@mail.com>
2022-10-15 15:46:14 +0530

    Adding Header UI and Layout

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./HeaderCartButton.module.css";
+
+import CartIcon from "../Cart/CartIcon";
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Cart</span>
+      <span className={classes.badge}></span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
